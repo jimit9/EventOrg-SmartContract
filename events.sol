86b63bedaa28541f8b577706d06d3b472cde8b4b
jimit <jimit@Jimits-MacBook-Air.local>
2023-06-01 14:25:40 +0530

    initial commit

---
+++ events.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 < 0.9.0;
+contract eventContract{
+struct Event{
+    address organizer;
+    string name;
+    uint date;
+    uint price;
+    uint ticketCount;
+    uint ticketRemaining;
+}
+}
