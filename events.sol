91fddd9d83916c455d57bc4e35bcb0a0082c4453
jimit <jimit@Jimits-MacBook-Air.local>
2023-06-15 12:05:12 +0530

    final commit

--- events.sol
+++ events.sol
@@ -13,4 +13,26 @@ struct Event{
 mapping (uint => Event) public events;
 mapping (address =>mapping(uint => uint)) public tickets;
 uint public nextId;
+function createEvent(string memory _name,uint _date,uint _price,uint _ticketCount) public {
+    require(_date>block.timestamp,"Organize event for future date");
+    
+    events[nextId] = Event(msg.sender,_name,_date,_price,_ticketCount,_ticketCount);
+    nextId++;
+}
+function buyTickets(uint id,uint quantity) public payable{
+require(events[id].date!=0,"Event doesnt exist");
+require(events[id].date>block.timestamp,"Event has already occured");
+Event storage _event=events[id];
+require(msg.value>=(_event.price*quantity),"Not enough money sent");
+require(_event.ticketRemaining>=quantity,"not enough tickets available");
+_event.ticketRemaining-=quantity;
+tickets[msg.sender][id]+=quantity;
+}
+function transferTickets(uint id,uint quantity,address receiver) public {
+require(events[id].date!=0,"Event doesnt exist");
+require(events[id].date>block.timestamp,"Event has already occured");
+require(tickets[msg.sender][id]>=quantity,"you do not have enough tickets");
+tickets[msg.sender][id]-=quantity;
+tickets[receiver][id]+=quantity;
+}
 }
